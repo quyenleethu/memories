00aad34a6c673fe0e9e25cffdd93bd81c58e82aa
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-04-19 03:00:58 +0700

    use Microlens logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   siteMetadata: {
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-emilia-core/gatsby-config.js
     siteTitle: `Memories of Le Thu Quyen`,
     siteTitleAlt: `Memories of Le Thu Quyen`,
     siteHeadline: `Memories of Le Thu Quyen`,
     siteUrl: `https://quyenleethu.github.io/memories`,
-    siteDescription: "Minimalistic portfolio/photography site with masonry grid, page transitions and big images",
+    siteDescription:
+      "Minimalistic portfolio/photography site with masonry grid, page transitions and big images",
     siteImage: `/banner.jpg`,
-    author: `@retro90s`,
+    author: `@microlens`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -15,10 +15,12 @@ module.exports = {
       resolve: `@lekoarts/gatsby-theme-emilia`,
       options: {
         name: `Le Thu Quyen`,
-        socialMedia: [{
-          title: `Instagram`,
-          href: `https://www.instagram.com/quyquy.9x`
-        }],
+        socialMedia: [
+          {
+            title: `Instagram`,
+            href: `https://www.instagram.com/quyquy.9x`,
+          },
+        ],
         showThemeAuthor: false,
         location: `Vietnam`,
       },
@@ -33,13 +35,10 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Memories of Le Thu Quyen`,
-        short_name: `Memories of Q`,
+        short_name: `MoQ`,
         description: `Memories of Le Thu Quyen`,
         start_url: `/`,
         background_color: `#fff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#3182ce`
         display: `standalone`,
         icons: [
           {
@@ -48,12 +47,12 @@ module.exports = {
             type: `image/png`,
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
+            src: `/android-chrome-384x384.png`,
+            sizes: `384x384`,
             type: `image/png`,
           },
         ],
       },
     },
   ].filter(Boolean),
-}
+};
